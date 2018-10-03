a60071e226418ffdfa92824a2a4fea1a09380cbb
Oliver Widder <oliver.widder@gmail.com>
2018-10-03 18:47:41 +0200

    HtmlSlide (1)

--- src/HtmlSlide.tsx
+++ src/HtmlSlide.tsx
@@ -1,14 +1,22 @@
 import * as React from 'react';
+import * as $ from 'jquery';
 
 interface HtmlSlideProps {
     pathToHtml: string,
+    name: string,
 }
 
 export class HtmlSlide extends React.Component<HtmlSlideProps> {
 
+    private container = React.createRef<HTMLDivElement>();
+
+    public componentDidMount() {
+        $(this.container).load(this.props.pathToHtml);
+    }
+
     public render() {
         return (
-            <div className="html-slide"></div>
+            <div id={this.props.name} ref={this.container} className="html-slide"></div>
         );
     }
 }
