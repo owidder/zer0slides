79799d247cdfbeb779a566132f39b4ed14166a85
Oliver Widder <oliver.widder@gmail.com>
2018-10-03 16:09:30 +0200

    rename App => GapSlides

--- src/GapSlides.tsx
+++ src/GapSlides.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import './App.css';
+import './GapSlides.css';
 
 import logo from './logo.svg';
 
-class App extends React.Component {
+class GapSlides extends React.Component {
   public render() {
     return (
       <div className="App">
@@ -19,4 +19,4 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default GapSlides;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+import GapSlides from './GapSlides';
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <App />,
+  <GapSlides />,
   document.getElementById('root') as HTMLElement
 );
-registerServiceWorker();
