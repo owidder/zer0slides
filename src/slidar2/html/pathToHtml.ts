95d25289875aff22b6341cbb97315e8cabf95fbb
Oliver Widder <oliver.widder@gmail.com>
2018-10-09 21:42:14 +0200

    img with flex

--- src/slidar2/html/pathToHtml.ts
+++ src/slidar2/html/pathToHtml.ts
@@ -1,11 +1,17 @@
 import {getFolderName} from '../core/core';
 
-export const pathToHtml = (name: string) => {
+export const pathTo = (name: string, extension?: string) => {
     const folderName = getFolderName();
 
+    const fullname = extension ? `${name}.${extension}` : name;
+
     if(!folderName) {
-        return `slides/${name}.html`;
+        return `slides/${fullname}`;
     }
 
-    return `slides/${folderName}/html/${name}.html`
+    return `slides/${folderName}/html/${fullname}`
+}
+
+export const pathToHtml = (name: string) => {
+    return pathTo(name, "html");
 }
