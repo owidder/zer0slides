1d6028549b690b4a0c46588a077fe0e6153977bb
Oliver Widder <oliver.widder@gmail.com>
2018-11-13 19:20:52 +0100

    debug (1)

--- src/index.tsx
+++ src/index.tsx
@@ -67,7 +67,7 @@ const bindKeys = () => {
     bindKeyToFunction("b", () => switchCurrentSlideToBlack());
 }
 
-initTooltip();
+//initTooltip();
 init();
 
 const initName = getParamValue("init", true);
