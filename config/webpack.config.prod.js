52364011e204b3d37b25379709a451ac81596dac
Oliver Widder <oliver.widder@gmail.com>
2019-01-20 13:49:59 +0100

    new extension for special slides: .special.html

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -239,6 +239,10 @@ module.exports = (folderName = '.') => {
                             ),
                             // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
                         },
+                        {
+                            test: /\.special.html$/,
+                            use: 'raw-loader'
+                        },
                         // "file" loader makes sure assets end up in the `build` folder.
                         // When you `import` an asset, you get its filename.
                         // This loader doesn't use a "test" so it will catch all modules
