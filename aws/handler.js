0ff36c9c24afe78f3274120ddad9ab8ab2d6c8ba
Oliver Widder <oliver.widder@gmail.com>
2019-06-09 09:38:58 +0200

    sandbox

--- aws/handler.js
+++ aws/handler.js
@@ -16,6 +16,8 @@ const successfullResponse = {
 const fetch = require("node-fetch");
 fetch.Promise = Bluebird;
 
+const Z0CONNECTION_TABLE = process.env.Z0CONNECTION_TABLE
+
 const nowAsString = () => {
     const now = new Date();
     return now.toString() + " ms: " + String(now.getMilliseconds());
@@ -24,6 +26,19 @@ const nowAsString = () => {
 const response = (statusCode, body) => {
     return {statusCode, body}
 }
+
+const ddbCall = (fct, params) => {
+    console.log(`ddbCall: ${fct}`);
+
+    return DDB[fct](params, (err, data) => {
+        if(err) {
+            console.log(err);
+        }
+        console.log(data);
+    }).promise();
+}
+
+const connectionIdFromEvent = event => event.requestContext.connectionId;
 
 const putItem = (tableName, item) => {
     const timestamp = nowAsString();
@@ -35,17 +50,12 @@ const putItem = (tableName, item) => {
         Item: itemWithTimestamp
     };
 
-    return DDB.putItem(putParams, function(err, data) {
-        if(err) {
-            console.log("err: " + err);
-        }
-        console.log("data: " + JSON.stringify(data));
-    }).promise();
+    return ddbCall('putItem', putParams);
 }
 
 const connect = async (event, context, callback) => {
-    await putItem(process.env.Z0CONNECTION_TABLE, {
-        connectionId: {S: event.requestContext.connectionId},
+    await putItem(Z0CONNECTION_TABLE, {
+        connectionId: {S: connectionIdFromEvent(event)},
         syncId: {S: "N/A"}
     });
 
@@ -54,13 +64,13 @@ const connect = async (event, context, callback) => {
 
 const disconnect = async (event, context, callback) => {
     const deleteParams = {
-        TableName: process.env.Z0CONNECTION_TABLE,
+        TableName: Z0CONNECTION_TABLE,
         Key: {
-            connectionId: {S: event.requestContext.connectionId}
+            connectionId: {S: connectionIdFromEvent(event)}
         }
     };
 
-    await DDB.deleteItem(deleteParams).promise();
+    await ddbCall('deleteItem', deleteParams);
 
     callback(null, response(200, "DISCONNECTED"));
 }
@@ -71,8 +81,8 @@ const register = async (event, context, callback) => {
     const body = JSON.parse(event.body);
     console.log(`syncId: ${body.syncId}`)
 
-    await putItem(process.env.Z0CONNECTION_TABLE, {
-        connectionId: {S: event.requestContext.connectionId},
+    await putItem(Z0CONNECTION_TABLE, {
+        connectionId: {S: connectionIdFromEvent(event)},
         syncId: {S: body.syncId}
     });
 
@@ -83,7 +93,24 @@ const defaultMessage = (event, context, callback) => {
     callback(null);
 };
 
+const getSyncIdForConnectionId = async (connectionId) => {
+    const params = {
+        Key: {connectionId: {S: connectionId}},
+        TableName: process.env.Z0CONNECTION_TABLE
+    }
+
+    const result = await ddbCall('getItem', params);
+
+    console.log(`result: ${JSON.stringify(result)}`);
+}
+
 const sendCommand = async (event, context, callback) => {
+    await getSyncIdForConnectionId(connectionIdFromEvent(event));
+
+    callback(null, response(200, "COMMAND SENT"));
+}
+
+const __sendCommand = async (event, context, callback) => {
     console.log("sendMessage");
     console.log("event: " + JSON.stringify(event));
     console.log("context: " + JSON.stringify(context));
