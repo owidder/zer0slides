3a162ed43867dac115ce3f0de081687ae39e0516
Oliver Widder <oliver.widder@gmail.com>
2019-06-27 10:53:34 +0200

    create test for error with slide-parameter

--- testcafe/smoketest.js
+++ testcafe/smoketest.js
@@ -1,9 +1,11 @@
-import { Selector } from 'testcafe';
+import {Selector, ClientFunction} from 'testcafe';
 
 fixture `Smoketest`
     .page `http://localhost:9000/smoketest/start.html`;
 
-test("Show bubbles", async t => {
+const getHash = ClientFunction(() => window.location.hash);
+
+test("Show bubbles and change slides", async t => {
     await t
         .click(".controlButton.down")
         .expect(Selector(".protip-content").withText("One").with({visibilityCheck: true}).exists).ok()
@@ -11,10 +13,25 @@ test("Show bubbles", async t => {
         .expect(Selector(".protip-content").withText("Two").with({visibilityCheck: true}).exists).ok()
         .click(".controlButton.down")
         .expect(Selector(".protip-content").withText("Three").with({visibilityCheck: true}).exists).ok()
+        .click(".controlButton.right")
+        .expect(Selector(".slideno.counter").innerText).eql("1")
+        .click(".controlButton.down")
+        .expect(Selector(".protip-content").withText("a").with({visibilityCheck: true}).exists).ok()
+        .click(".controlButton.down")
+        .expect(Selector(".protip-content").withText("b").with({visibilityCheck: true}).exists).ok()
+        .click(".controlButton.down")
+        .expect(Selector(".protip-content").withText("c").with({visibilityCheck: true}).exists).ok()
+        .click(".controlButton.left")
+        .expect(Selector(".protip-content").withText("Three").with({visibilityCheck: true}).exists).ok()
+        .click(".controlButton.right")
+        .expect(Selector(".protip-content").withText("c").with({visibilityCheck: true}).exists).ok()
 })
 
-test("Next slide", async t => {
+test("Navidate via content slide", async t => {
     await t
-        .click(".controlButton.right")
-        .expect(Selector(".slideno.counter").innerText).eql("1");
-});
+        .click(".controlButton.left")
+        .click(".controlButton.down")
+        .click(".controlButton.down")
+        .pressKey("enter")
+        .expect(getHash()).contains("slide=1")
+})
