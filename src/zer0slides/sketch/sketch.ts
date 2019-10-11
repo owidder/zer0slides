1eecb386ae6676b1832a89ecbf79c84cafcb0e11
Oliver Widder <oliver.widder@gmail.com>
2019-10-11 17:51:31 +0200

    rectProzFromRect

--- src/zer0slides/sketch/sketch.ts
+++ src/zer0slides/sketch/sketch.ts
@@ -2,9 +2,24 @@ import * as Vivus from "vivus";
 import * as d3 from "d3";
 
 import {slideName} from '../core/core';
-import {q} from "../selector/selector";
 
 const rough = require("roughjs/dist/rough.umd");
+
+const screenWidth = window.innerWidth;
+const screenHeight = window.innerHeight;
+
+interface Rect {
+    upperLeftX: number,
+    upperLeftY: number,
+    width: number,
+    height: number
+}
+
+interface Options {
+    fill?: string,
+    fillStyle?: string,
+    roughness?: number
+}
 
 class Sketch {
 
@@ -31,17 +46,40 @@ class Sketch {
         })
     }
 
-    async rect(id: string, upperLeftX: number, upperLeftY: number, width: number, height: number, text?: string, fill = "pink", fillStyle?: string) {
-        const node = this.r.rectangle(upperLeftX, upperLeftY, width, height, {fill, fillStyle, roghness: 3});
+    rectProzFromRect(rect: Rect, id: string, upperLeftXProz: number, upperLeftYProz: number, widthProz: number, heightProz: number, text?: string, options: Options = {}): Rect {
+        const upperLeftX = rect.width * upperLeftXProz + rect.upperLeftX;
+        const upperLeftY = rect.height * upperLeftYProz + rect.upperLeftY;
+        const width = rect.width * widthProz;
+        const height = rect.height * heightProz;
+
+        this.rect(id, upperLeftX, upperLeftY, width, height, text, options);
+
+        return {upperLeftX, upperLeftY, width, height}
+    }
+
+    rectProz(id: string, upperLeftXProz: number, upperLeftYProz: number, widthProz: number, heightProz: number, text?: string, options: Options = {}): Rect {
+        const upperLeftX = screenWidth * upperLeftXProz;
+        const upperLeftY = screenHeight * upperLeftYProz;
+        const width = screenWidth * widthProz;
+        const height = screenHeight * heightProz;
+
+        this.rect(id, upperLeftX, upperLeftY, width, height, text, options);
+
+        return {upperLeftX, upperLeftY, width, height}
+    }
+
+    async rect(id: string, upperLeftX: number, upperLeftY: number, width: number, height: number, text: string,
+               {fill = "pink", fillStyle = "solid", roughness = 3}: Options) {
+        const node = this.r.rectangle(upperLeftX, upperLeftY, width, height, {fill, fillStyle, roughness});
         const qid = await this.add(node, id);
 
-        if(text) {
+        if (text) {
             d3.select(`#${qid}`)
                 .append("text")
                 .attr("class", "rect-text")
                 .attr("font-family", "Mansalva")
-                .attr("x", 20)
-                .attr("y", 50)
+                .attr("x", upperLeftX + 20)
+                .attr("y", upperLeftY + 50)
                 .text(text);
         }
     }
